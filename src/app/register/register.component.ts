db13d360276503ed03b800f1c34d2d3de82043dc
Kanumuru <AG22146@ad.wellpoint.com>
2021-10-06 18:43:53 +0530

    Snackbar config changes

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
 import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { AppService } from '../app.service';
@@ -29,7 +29,9 @@ export class RegisterComponent implements OnDestroy {
   email = new FormControl('', [Validators.required, Validators.email]);
   matcher = new MyErrorStateMatcher();
   user: User = new User();
-  duration = 1500;
+  duration = 2000;
+  action = 'Go To Home';
+  snackBarConfig: MatSnackBarConfig = { duration: this.duration, horizontalPosition: 'right' };
   subscriptions: Subscription[] = [];
   constructor(private ref: ChangeDetectorRef, public router: Router, private appService: AppService, private snackBar: MatSnackBar) {
     this.isRegisterRoute = router.url.split('/')[1] === 'register';
@@ -43,7 +45,7 @@ export class RegisterComponent implements OnDestroy {
     this.user.email = this.email.value;
     this.user.imageURL = this.imageUrl;
     if (this.user.email && this.user.imageURL) {
-      this.snackBar.open('Registration successful!', 'Go To Home', { duration: this.duration }).afterDismissed().subscribe(() => {
+      this.snackBar.open('Registration successful!', this.action, this.snackBarConfig).afterDismissed().subscribe(() => {
         this.appService.sendEmailID(this.user.email);
       });
       const subscription = this.appService.registerUser(this.user.email, this.user.imageURL).subscribe((data) => {
@@ -58,7 +60,7 @@ export class RegisterComponent implements OnDestroy {
     this.user.email = this.email.value;
     this.user.imageURL = this.imageUrl;
     if (this.user.email && this.user.imageURL) {
-      this.snackBar.open('Login successful!', 'Go To Home', { duration: this.duration }).afterDismissed().subscribe(() => {
+      this.snackBar.open('Login successful!', this.action, this.snackBarConfig).afterDismissed().subscribe(() => {
         this.appService.sendEmailID(this.user.email);
       });
     }
